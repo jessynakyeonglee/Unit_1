71b1ce7e4f0f93a823ab63f6eac6bb526fa80a8a
leena <leena@ssfs.org>
2019-09-09 14:13:03 -0400

    added hellolockthemfglock

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello.world");
         System.out.println("Hello.lockthemfglock");
         System.out.println("This is \"Jessy's\"computer");
-
+        System.out.println("2:09 pm");
+        System.out.println("9/9/2019");
     }
 }
