f1b6ea6edfb70b46d6ae342985ac9651d403b85a
leena <leena@ssfs.org>
2019-09-10 15:08:44 -0400

    finished part 1

---
+++ src/com/company/Escape.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("What is the difference between");
+        System.out.print("and a "?");
+                System.out.print
+
+    }
+}
