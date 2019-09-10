34a319592a124932e2a38aef20ece2b6a8925005
leena <leena@ssfs.org>
2019-09-10 14:55:23 -0400

    finished part 1

--- src/com/company/printing.java
+++ src/com/company/printing.java
@@ -1,7 +1,7 @@
 package com.company;
 public class printing {
     public static void main(String[] args) {
-        System.out.print("9/10/19");
+        System.out.println("9/10/19");
         System.out.println("2:48");
         System.out.println("It is a great");
         System.out.println("time to be a");
