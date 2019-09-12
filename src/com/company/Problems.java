5904ab596860f084580258154d713655a6edcd4c
leena <leena@ssfs.org>
2019-09-12 08:58:12 -0400

    finished part 1 and 2

---
+++ src/com/company/Problems.java
@@ -0,0 +1,20 @@
+// ********************************************
+//   Problems.java
+//
+//   There are several error in the code below. Fix them all to get things to run
+//   Hopefully you can see that IDEA can be a huge help with this.
+//
+ ********************************************
+
+public class problems
+{
+    public Static main (string[] args)
+    {
+        System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println (This program used to have lots of problems,");
+                System.0ut.println ("but if it prints this, you fixed them all.")
+        System.out.println ("         	*** Hurray! ***);
+                System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
+
