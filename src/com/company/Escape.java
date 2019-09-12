b426388ab5b508f3a2bb42f2754853ddaedf6c92
leena <leena@ssfs.org>
2019-09-12 08:41:56 -0400

    finished part 1

--- src/com/company/Escape.java
+++ src/com/company/Escape.java
@@ -3,8 +3,17 @@ package com.company;
 public class Escape {
     public static void main(String[] args) {
         System.out.println("What is the difference between");
-        System.out.print("and a "?");
-                System.out.print
+        System.out.println("a 'and a \"? or between a \"and a\"?" );
+        System.out.println("One is what we see when we're typing our program.");
+        System.out.println("The other is what appears on the \"console.");
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+
+
 
     }
 }
