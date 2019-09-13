9b040556ea9a54aad1046a44e10a23780cb592b6
leena <leena@ssfs.org>
2019-09-13 10:15:42 -0400

    finished 9/13/19

--- src/com/company/Painting.java
+++ src/com/company/Painting.java
@@ -3,8 +3,21 @@ package com.company;
 public class Painting {
 
     public static void main(String[]args) {
-        int width 32
-        int length 40
-        int height 16
-    }
+        int width = 40;
+        int length = 32;
+        int height = 16;
+        int wall1 = width*height;
+        int wall2 = length*height;
+        int wall1s = wall1*2;
+        int wall2s = wall2*2;
+        int ceiling = width*length;
+        int total = wall1s+wall2s+ceiling;
+        int doors = 20*2;
+        int windows = 15*4;
+        int doorsandwindows = doors+windows;
+        int finale = total-doorsandwindows;
+        double finalefr = finale/350;
+        System.out.println ("You will need "+finalefr+" gallons of paint");
+
+}
 }
