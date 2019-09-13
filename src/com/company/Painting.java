d8802e96b35d1bc6c3598040edc345e4f403c6ec
leena <leena@ssfs.org>
2019-09-13 09:42:14 -0400

    finished error correction

---
+++ src/com/company/Painting.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Painting {
+
+    public static void main(String[]args) {
+        int width 32
+        int length 40
+        int height 16
+    }
+}
--- src/com/company/Problems.java
+++ src/com/company/Problems.java
@@ -4,17 +4,18 @@
 //   There are several error in the code below. Fix them all to get things to run
 //   Hopefully you can see that IDEA can be a huge help with this.
 //
- ********************************************
 
-public class problems
-{
-    public Static main (string[] args)
-    {
-        System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        System.out.println (This program used to have lots of problems,");
-                System.0ut.println ("but if it prints this, you fixed them all.")
-        System.out.println ("         	*** Hurray! ***);
-                System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    }
+public class Problems {
+    public static void main(String[]args){
+
+        }
+        {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("This program used to have lots of problems, ");
+        System.out.println("but if it prints this, you fixed them all.");
+        System.out.println("         	*** Hurray! ***");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        }
 }
 
