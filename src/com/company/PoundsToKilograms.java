9d05a7f485e9ac27ff5c2214cdbb0121a36c5310
leena <leena@ssfs.org>
2019-09-16 11:06:35 -0400

    finished 9/16

---
+++ src/com/company/PoundsToKilograms.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class PoundsToKilograms {
+    public static void main(String[]args){
+        double lbs = 115;
+        double kg = lbs/2.205;
+        System.out.print(lbs+" pounds =");
+        System.out.print(kg+"kilograms");
+    }
+}
---
+++ src/com/company/TotalWithTax.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class TotalWithTax {
+    public static void main (String[]args){
+        double TAXRATE = 0.0825;
+        double price = 52.75;
+        double salestax = price*TAXRATE;
+        double total = price+salestax;
+        System.out.println("Price: $"+price);
+        System.out.println("Sales Tax: $"+salestax);
+        System.out.print("Total: $"+total);
+
+
+    }
+}
