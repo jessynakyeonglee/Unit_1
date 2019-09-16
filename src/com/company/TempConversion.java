8febdfb1328e2a2c7162c593e4330255a9207a74
leena <leena@ssfs.org>
2019-09-16 11:17:10 -0400

    finished 9/16

---
+++ src/com/company/TempConversion.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class TempConversion {
+    public static void main(String[] args) {
+        double far = 79;
+        double cel = (far - 32) * 5 / 9;
+        System.out.println("Fahrenheit: " + far);
+        System.out.print("Celsius: " + cel);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
