eb953a348d2202f8dac939a361af8ab961c76d07
leena <leena@ssfs.org>
2019-09-17 12:49:02 -0400

    made scanner for TotalWithTax 9/17/19

--- src/com/company/TotalWithTax.java
+++ src/com/company/TotalWithTax.java
@@ -1,15 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TotalWithTax {
     public static void main (String[]args){
+Scanner tempo=new Scanner(System.in);
+        System.out.println("Enter the price of your item");
 
         final double TAXRATE = 0.0825;
-        double price = 52.75;
+        double price = tempo.nextInt();
         double salestax = price*TAXRATE;
         double total = price+salestax;
         System.out.println("Price: $"+price);
         System.out.println("Sales Tax: $"+salestax);
         System.out.print("Total: $"+total);
+
 
 
     }
