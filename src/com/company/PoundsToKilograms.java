a0d7c4f3fb4ca62dc8284e22ae5a5045ecbc1af2
leena <leena@ssfs.org>
2019-09-17 12:41:37 -0400

    made scanner for poundstokilograms 9/17/19

--- src/com/company/PoundsToKilograms.java
+++ src/com/company/PoundsToKilograms.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PoundsToKilograms {
     public static void main(String[]args){
-        double lbs = 115;
+        Scanner snapple=new Scanner(System.in);
+        System.out.println("Enter the number of pounds");
+
+        double lbs = snapple.nextInt();
         double kg = lbs/2.205;
         System.out.print(lbs+" pounds =");
         System.out.print(kg+"kilograms");
