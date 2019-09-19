564de47e4df6964b5fc55ebd74833ad833ee7721
leena <leena@ssfs.org>
2019-09-19 15:42:28 -0400

    9/19/19 E block

---
+++ src/com/company/RoundTenth.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class RoundTenth {
+    public static void main (String[]args){
+        Scanner tenten=new Scanner(System.in);
+        System.out.println("Decimal number?");
+        double givenum = tenten.nextDouble();
+times ten plus point five and devide by ten when done
+    }
+}
